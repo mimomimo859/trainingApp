77b8254b55cdd6dfa15aa8e10ecd1804e70d3ec4
mimomimo859 <104977028+mimomimo859@users.noreply.github.com>
2022-12-11 20:00:46 +0900

    Initial Commit

---
+++ trainingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  trainingApp
+//
+//  Created by 村田　美壮 on 2022/12/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
